195299fe889a285e243414daec4024d0e4a712ee
Anthony <antnay@uw.edu>
2026-01-22 17:09:12 -0800

    feat: header and question parsing

--- backend/src/handler/query.rs
+++ backend/src/handler/query.rs
@@ -1,31 +1,53 @@
-use std::{iter::Inspect, net::IpAddr, time::Instant};
+use std::{net::IpAddr, time::Instant};
 
 use tokio::sync::mpsc;
 
+use crate::handler::resolver::Resolver;
+
 #[derive(thiserror::Error, Debug)]
-pub enum HandlerError {}
+pub enum HandlerError {
+    #[error("failed to parse query: {0}")]
+    Parse(String),
+    #[error("resolver error: {0}")]
+    Resolver(#[from] crate::handler::resolver::ResolverError),
+}
+
+pub enum QueryType {
+    A,
+}
 
 pub struct QueryHandler {
     resolver: Resolver,
-    logger_tx: mpsc::Sender<Event>,
+    // logger_tx: mpsc::Sender<Event>,
 }
 
 impl QueryHandler {
-    pub fn new(resolver: Resolver, logger_tx: mpsc::Sender<Event>) -> Self {
+    pub fn new(
+        resolver: Resolver,
+        // _logger_tx: mpsc::Sender<Event>
+    ) -> Self {
         Self {
             resolver,
-            logger_tx,
+            // logger_tx,
         }
     }
 
-    pub async fn handle(&self, data: &[u8], client: IpAddr) {
-        // let begin = Instant::now();
-
-        // blocklist
-        // cache
-        // upstream
-
+    pub async fn handle(&self, data: &[u8], client: IpAddr) -> Result<Vec<u8>, HandlerError> {
+        println!("incoming client: {}", client);
+        let begin = Instant::now();
+        let query = self.resolver.parse(data).await?;
+        self.resolver.resolve(&query).await?;
+        let delta = begin.elapsed();
+        println!("query time: {:?}", delta);
         // record statistics
         // log
+        Ok("response".as_bytes().to_vec())
     }
+}
+
+pub struct Query {
+    pub id: u16,
+    pub name: String,
+    pub query_type: QueryType,
+    pub raw: Vec<u8>,
 }
--- backend/src/handler/resolver.rs
+++ backend/src/handler/resolver.rs
@@ -1,4 +1,112 @@
-#[derive(thiserror::Error, Debug)]
-pub enum ResolverError {}
+use std::fmt::LowerHex;
 
-pub struct Resolver {}
+use clap::builder::Str;
+
+use crate::handler::UpstreamError;
+use crate::handler::UpstreamPool;
+use crate::handler::UpstreamResponse;
+
+use crate::handler::query::Query;
+use crate::handler::query::QueryType;
+
+const QNAME_START: usize = 12;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ResolverError {
+    #[error("upstream error: {0}")]
+    Upstream(UpstreamError),
+    #[error("{1} parsing error: {0}")]
+    UTF8ToString(std::string::FromUtf8Error, String),
+}
+
+impl From<UpstreamError> for ResolverError {
+    fn from(err: UpstreamError) -> Self {
+        ResolverError::Upstream(err)
+    }
+}
+
+enum ParseState {
+    Length,
+    Scan,
+}
+
+pub struct Resolver {
+    // cache: Cache,
+    // blocklist: Arc<Blocklist>
+    upstream: UpstreamPool,
+}
+
+impl Resolver {
+    pub fn new(upstream: UpstreamPool) -> Self {
+        Self { upstream }
+    }
+
+    pub async fn parse(&self, data: &[u8]) -> Result<Query, ResolverError> {
+        let id = u16::from_be_bytes([data[0], data[1]]);
+        let qname = self.parse_question(data);
+
+        let qname_str = String::from_utf8_lossy(&qname);
+        if cfg!(debug_assertions) {
+            println!("qname bytes: {:?}", qname);
+            println!("qname string: {}", qname_str.to_string());
+        }
+
+        Ok(Query {
+            id,
+            name: qname_str.to_string(),
+            query_type: QueryType::A,
+            raw: data.to_vec(),
+        })
+    }
+
+    // #[inline]
+    // async fn parse_header(&self, data: &[u8]) {
+    //     let id = u16::from_be_bytes([data[0], data[1]]);
+    // }
+
+    #[inline]
+    fn parse_question(&self, data: &[u8]) -> Vec<u8> {
+        let mut indx = QNAME_START;
+        let mut len = 0;
+        let mut state = ParseState::Length;
+        let mut buf = Vec::with_capacity(64);
+
+        while data[indx] != 0x00 {
+            match state {
+                ParseState::Length => {
+                    len = data[indx];
+                    indx += 1;
+                    state = ParseState::Scan
+                }
+                ParseState::Scan => {
+                    let stop = indx + len as usize;
+                    for i in indx..stop {
+                        println!(
+                            "indx:{} | i: {} | data: '{}' - {} | len: {}",
+                            indx, i, data[i as usize] as char, data[i as usize], len
+                        );
+                        buf.push(data[i as usize]);
+                    }
+                    indx += len as usize;
+                    if data[indx] != 0x00 {
+                        buf.push(46);
+                    }
+                    state = ParseState::Length;
+                }
+            }
+        }
+        buf
+    }
+
+    // #[inline]
+    // async fn parse_answer(&self, data: &[u8]) {}
+
+    pub async fn resolve(&self, query: &Query) -> Result<UpstreamResponse, ResolverError> {
+        // bloclist
+        // cache
+        println!("querying {}", query.name);
+        let res = self.upstream.resolve(query).await?;
+        // insert into cache
+        Ok(res)
+    }
+}
